14c3358f7de8d14da07c8a2add8719f272e32347
qtrong0205 <quoctrong02052006@gmail.com>
2026-05-30 18:42:12 +0700

    Update data.sql with absolute paths for CSV files

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 -- ============================================================
 -- BULK INSERT - QuanLiChuyenBay
 -- Chay file nay sau khi da tao schema (QuanLyChuyenBay.sql)
--- Thay the <DUONG_DAN_THU_MUC> bang duong dan tuyet doi chua cac file CSV
+-- Thay the D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\ bang duong dan tuyet doi chua cac file CSV
 -- Vi du Windows: C:\Data\csv_data\
 -- Vi du Linux:   /home/user/csv_data/
 -- ============================================================
@@ -13,7 +13,7 @@ GO
 -- 1. SANBAY (30 rows) - khong co FK, insert truoc
 -- ============================================================
 BULK INSERT dbo.SANBAY
-FROM '<DUONG_DAN_THU_MUC>SANBAY.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\SANBAY.csv'
 WITH (
     FIRSTROW       = 2,
     FIELDTERMINATOR = ',',
@@ -28,7 +28,7 @@ GO
 -- 2. HANGHANGKHONG (15 rows) - khong co FK
 -- ============================================================
 BULK INSERT dbo.HANGHANGKHONG
-FROM '<DUONG_DAN_THU_MUC>HANGHANGKHONG.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\HANGHANGKHONG.csv'
 WITH (
     FIRSTROW       = 2,
     FIELDTERMINATOR = ',',
@@ -43,7 +43,7 @@ GO
 -- 3. LOAIMAYBAY (10 rows) - khong co FK
 -- ============================================================
 BULK INSERT dbo.LOAIMAYBAY
-FROM '<DUONG_DAN_THU_MUC>LOAIMAYBAY.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\LOAIMAYBAY.csv'
 WITH (
     FIRSTROW       = 2,
     FIELDTERMINATOR = ',',
@@ -58,7 +58,7 @@ GO
 -- 4. MAYBAY (100 rows) - FK -> LOAIMAYBAY, HANGHANGKHONG
 -- ============================================================
 BULK INSERT dbo.MAYBAY
-FROM '<DUONG_DAN_THU_MUC>MAYBAY.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\MAYBAY.csv'
 WITH (
     FIRSTROW       = 2,
     FIELDTERMINATOR = ',',
@@ -73,11 +73,11 @@ GO
 -- 5. CHUYENBAY (500 rows) - FK -> SANBAY (x2)
 -- ============================================================
 BULK INSERT dbo.CHUYENBAY
-FROM '<DUONG_DAN_THU_MUC>CHUYENBAY.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\CHUYENBAY.csv'
 WITH (
     FIRSTROW       = 2,
     FIELDTERMINATOR = ',',
-    ROWTERMINATOR  = '0x0a',
+    ROWTERMINATOR = '0x0d0a',
     CODEPAGE       = '65001'
 );
 GO
@@ -88,7 +88,7 @@ GO
 -- 6. NHANVIEN (2000 rows) - khong co FK
 -- ============================================================
 BULK INSERT dbo.NHANVIEN
-FROM '<DUONG_DAN_THU_MUC>NHANVIEN.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\NHANVIEN.csv'
 WITH (
     FIRSTROW       = 2,
     FIELDTERMINATOR = ',',
@@ -104,7 +104,7 @@ GO
 -- SoHoChieu co the NULL => dung format file xu ly cot empty
 -- ============================================================
 BULK INSERT dbo.HANHKHACH
-FROM '<DUONG_DAN_THU_MUC>HANHKHACH.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\HANHKHACH.csv'
 WITH (
     FIRSTROW        = 2,
     FIELDTERMINATOR = ',',
@@ -120,7 +120,7 @@ GO
 -- 8. LICHBAY (2000 rows) - FK -> CHUYENBAY, MAYBAY
 -- ============================================================
 BULK INSERT dbo.LICHBAY
-FROM '<DUONG_DAN_THU_MUC>LICHBAY.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\LICHBAY.csv'
 WITH (
     FIRSTROW       = 2,
     FIELDTERMINATOR = ',',
@@ -136,7 +136,7 @@ GO
 --    Insert truoc BANGGIAVE vi khong phu thuoc nhau
 -- ============================================================
 BULK INSERT dbo.BANGPHANCONG
-FROM '<DUONG_DAN_THU_MUC>BANGPHANCONG.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\BANGPHANCONG.csv'
 WITH (
     FIRSTROW       = 2,
     FIELDTERMINATOR = ',',
@@ -153,7 +153,7 @@ GO
 --     Tat ca du lieu da duoc kiem tra truoc: BatDau < KetThuc < ThoiGianDi
 -- ============================================================
 BULK INSERT dbo.BANGGIAVE
-FROM '<DUONG_DAN_THU_MUC>BANGGIAVE.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\BANGGIAVE.csv'
 WITH (
     FIRSTROW       = 2,
     FIELDTERMINATOR = ',',
@@ -179,7 +179,7 @@ GO
 -- Neus muon tat trigger trong luc insert thi dung cach 2 duoi day
 
 BULK INSERT dbo.VE
-FROM '<DUONG_DAN_THU_MUC>VE.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\VE.csv'
 WITH (
     FIRSTROW        = 2,
     FIELDTERMINATOR = ',',
@@ -198,7 +198,7 @@ DISABLE TRIGGER dbo.TR_VE_CheckTrangThai  ON dbo.VE;
 DISABLE TRIGGER dbo.TR_VE_CheckSucChua    ON dbo.VE;
 
 BULK INSERT dbo.VE
-FROM '<DUONG_DAN_THU_MUC>VE.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\VE.csv'
 WITH (
     FIRSTROW        = 2,
     FIELDTERMINATOR = ',',
@@ -215,7 +215,7 @@ ENABLE TRIGGER dbo.TR_VE_CheckSucChua    ON dbo.VE;
 -- 12. HANHLY (4782 rows) - FK -> VE
 -- ============================================================
 BULK INSERT dbo.HANHLY
-FROM '<DUONG_DAN_THU_MUC>HANHLY.csv'
+FROM 'D:\Hoc_Tap\HK4\IE103_QuanLyThongTin\code\đồ án\csv_data\HANHLY.csv'
 WITH (
     FIRSTROW       = 2,
     FIELDTERMINATOR = ',',
